23f7dd4d5eb5d4df24932a2110c86ff5eaf3d293
ShanRastogi22 <nash82826@gmail.com>
2023-08-09 08:46:40 +0530

    nav bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import { Component } from "react";
+import Ani from '../Img/ani.png';
+import { MenuData } from "./MenuData";
+import './NavStyle.css';
+
+
+class Navbar extends Component {
+    state = { clicked: false};
+    handleClick =() =>{
+        this.setState({clicked: !this.state.clicked})
+    }
+    render() {
+        return (
+            <nav className="NavbarItems">
+                <div className="logo">
+                <h1 className="">
+                    ANI
+                </h1>
+                <img src={Ani} className="img"/>
+                </div>
+                <div className="menu-icons" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                </div>
+                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+                    {MenuData.map((item, index) => {
+                        return(
+                            <li key={index}>
+                        <a href={item.url} className={item.cName}>
+                            <i className={item.icon}></i>{item.title}
+                        </a>
+                    </li>
+                        )
+                    })}
+                    
+                </ul>
+            </nav>
+        )
+    }
+}
+
+
+export default Navbar;
